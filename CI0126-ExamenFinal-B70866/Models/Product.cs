b662886d15e29bd6be54828df97cac14102d41ca
GIANFRANCO.BAGNARELLO <gianfranco.bagnarello@ucr.ac.cr>
2020-12-05 02:19:50 -0600

    Añadidos métodos HTMLHelper para la vista en el modelo Product. Añadidos métodos get y set a los atributos.

--- CI0126-ExamenFinal-B70866/Models/Product.cs
+++ CI0126-ExamenFinal-B70866/Models/Product.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace CI0126_ExamenFinal_B70866.Models
 {
     public class Product
     {
-        string name;
-        string description;
-        int discountAmount;
-        double price;
-        double discount;
-
-
+        [Required(ErrorMessage = "Es necesario que escriba el nombre del producto")]
+        [Display(Name = "Ingrese el nombre del producto")]
+        string name { get; set; }
+        [Required(ErrorMessage = "Es necesario que escriba la descripción del producto")]
+        [Display(Name = "Ingrese la descripción del producto")]
+        string description { get; set; }
+        [Required(ErrorMessage = "Es necesario que escriba la cantidad para aplicar descuento al producto")]
+        [Display(Name = "Ingrese la cantidad de unidades para aplicar descuento al producto")]
+        int discountAmount { get; set; }
+        [Required(ErrorMessage = "Es necesario que escriba el precio del producto")]
+        [Display(Name = "Ingrese el precio del producto")]
+        double price { get; set; }
+        [Required(ErrorMessage = "Es necesario que escriba el descuento aplicable al producto")]
+        [Display(Name = "Ingrese el descuento aplicable al producto")]
+        double discount { get; set; }
     }
 }
