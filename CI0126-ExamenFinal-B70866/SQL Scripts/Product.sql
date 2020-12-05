88602a19a1b5125a4c39731f9e0a683a4c457796
GIANFRANCO.BAGNARELLO <gianfranco.bagnarello@ucr.ac.cr>
2020-12-05 15:16:13 -0600

    Creada tabla Product en la base de datos local. Creado folder que contiene los SQL Queries para el proyecto.

---
+++ CI0126-ExamenFinal-B70866/SQL Scripts/Product.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Product(
+	productID INTEGER PRIMARY KEY IDENTITY,
+	productName NVARCHAR(MAX)  NOT NULL,
+	productDescription NVARCHAR(MAX) NOT NULL,
+	discountAmount INTEGER NOT NULL,
+	price FLOAT NOT NULL,
+	discount FLOAT NOT NULL,
+	productImage VARBINARY(MAX),
+	imageType NVARCHAR(50),
+)
