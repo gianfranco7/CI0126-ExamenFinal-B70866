4e26e9481fbc35510fdb3cab9d7276875d087af6
GIANFRANCO.BAGNARELLO <gianfranco.bagnarello@ucr.ac.cr>
2020-12-05 02:13:11 -0600

    Creado branch para la primera historia. Creada clase Product con sus respectivos atributos.

---
+++ CI0126-ExamenFinal-B70866/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CI0126_ExamenFinal_B70866.Models
+{
+    public class Product
+    {
+        string name;
+        string description;
+        int discountAmount;
+        double price;
+        double discount;
+
+
+    }
+}
