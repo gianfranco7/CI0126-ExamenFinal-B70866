203ae2f28da4086303c59d19b9faa83be5159891
GIANFRANCO.BAGNARELLO <gianfranco.bagnarello@ucr.ac.cr>
2020-12-05 02:26:57 -0600

    Creado Controlador para Producto. Creados métodos para añadir productos.

---
+++ CI0126-ExamenFinal-B70866/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CI0126_ExamenFinal_B70866.Models;
+
+namespace CI0126_ExamenFinal_B70866.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult addProduct() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult addProduct(Product product)
+        {
+            return View();
+        }
+    }
+}
