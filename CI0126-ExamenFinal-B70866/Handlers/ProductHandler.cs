a07451b74c42964951bd9c8e5ba859f9b010b9b0
GIANFRANCO.BAGNARELLO <gianfranco.bagnarello@ucr.ac.cr>
2020-12-05 16:26:37 -0600

    Añadida clase abstracta Handler, modificado handler de Productos.

--- CI0126-ExamenFinal-B70866/Handlers/ProductHandler.cs
+++ CI0126-ExamenFinal-B70866/Handlers/ProductHandler.cs
@@ -1,11 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using SqlKata.Execution;
 using System.Web;
+using System.IO;
+using Dapper;
+using CI0126_ExamenFinal_B70866.Models;
+using CI0126_ExamenFinal_B70866.Handlers;
 
-namespace CI0126_ExamenFinal_B70866.Handlers
+namespace comunidad_practica.Handlers
 {
-    public class ProductHandler
+    public class ProductHandler : Handler
     {
+        private byte[] getBytes(HttpPostedFileBase file)
+        {
+            byte[] bytes;
+            BinaryReader reader = new BinaryReader(file.InputStream);
+            bytes = reader.ReadBytes(file.ContentLength);
+            return bytes;
+        }
+
+        public void addProduct(Product newProduct)
+        {
+            try
+            {
+                db.Query("Product").Insert(new
+                {
+                    newProduct.name,
+                    newProduct.description,
+                    newProduct.discountAmount,
+                    newProduct.price,
+                    newProduct.discount,
+                    eventImage = getBytes(newProduct.image),
+                    fileType = newProduct.image.ContentType,
+                });
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
     }
 }
--- CI0126-ExamenFinal-B70866/Models/Product.cs
+++ CI0126-ExamenFinal-B70866/Models/Product.cs
@@ -8,22 +8,22 @@ namespace CI0126_ExamenFinal_B70866.Models
 {
     public class Product
     {
-        int id { get; set; }
+        public int id { get; set; }
         [Required(ErrorMessage = "Es necesario que escriba el nombre del producto")]
         [Display(Name = "Ingrese el nombre del producto")]
-        string name { get; set; }
+        public string name { get; set; }
         [Required(ErrorMessage = "Es necesario que escriba la descripción del producto")]
         [Display(Name = "Ingrese la descripción del producto")]
-        string description { get; set; }
+        public string description { get; set; }
         [Required(ErrorMessage = "Es necesario que escriba la cantidad para aplicar descuento al producto")]
         [Display(Name = "Ingrese la cantidad de unidades para aplicar descuento al producto")]
-        int discountAmount { get; set; }
+        public int discountAmount { get; set; }
         [Required(ErrorMessage = "Es necesario que escriba el precio del producto")]
         [Display(Name = "Ingrese el precio del producto")]
-        double price { get; set; }
+        public double price { get; set; }
         [Required(ErrorMessage = "Es necesario que escriba el descuento aplicable al producto")]
         [Display(Name = "Ingrese el descuento aplicable al producto")]
-        double discount { get; set; }
+        public double discount { get; set; }
         [Required(ErrorMessage = "Es necesario que añada una imagen al producto")]
         [Display(Name = "Ingrese una imagen para el producto")]
         public HttpPostedFileBase image { get; set; }
