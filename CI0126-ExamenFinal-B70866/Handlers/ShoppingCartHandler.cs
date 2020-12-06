b186ab66d2a464a04859869c6573a0df4578b67d
GIANFRANCO.BAGNARELLO <gianfranco.bagnarello@ucr.ac.cr>
2020-12-05 22:41:02 -0600

    A;ñadido método en el hadnler de carrito de compras para añadir productos al carrito.

--- CI0126-ExamenFinal-B70866/Handlers/ShoppingCartHandler.cs
+++ CI0126-ExamenFinal-B70866/Handlers/ShoppingCartHandler.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using SqlKata.Execution;
 using System.Web;
+using System.IO;
+using Dapper;
+using CI0126_ExamenFinal_B70866.Models;
+using Helpers;
 
 namespace CI0126_ExamenFinal_B70866.Handlers
 {
-    public class ShoppingCartHandler
+    public class ShoppingCartHandler : Handler
     {
+        public void addProductToCart(Product product)
+        {
+            try
+            {
+                db.Query("ShoppingCart").Insert(new
+                {
+                    productID = product.id,
+                    productName = product.name
+                });
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
     }
 }
