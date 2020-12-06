8f4142c8dbfbb63fa03282db8b4a519722deb32d
GIANFRANCO.BAGNARELLO <gianfranco.bagnarello@ucr.ac.cr>
2020-12-06 02:55:30 -0600

    Creados dos métodos de prueba en funciones importantes (comprar productos y conseguir los IDs de forma ordenada)

---
+++ CI0126-ExamenFinal-B70866.Tests/Controllers/ProductControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CI0126_ExamenFinal_B70866.Controllers;
+using System.Web.Mvc;
+using CI0126_ExamenFinal_B70866.Models;
+using System.Collections.Generic;
+
+namespace CI0126_ExamenFinal_B70866.Tests.Controllers
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        [TestMethod]
+        public void buyProductsTest()
+        {
+            // Arrange
+            ProductController controller = new ProductController();
+
+            // Act
+            ViewResult result = controller.buyProducts() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void getOrderedDistinctIDsTest()
+        {
+            // Arrange
+            List<Product> testProductList = new List<Product>();
+            ProductController controller = new ProductController();
+
+            // Act
+            List<int>result = controller.getOrderedDistinctIDs(testProductList);
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
