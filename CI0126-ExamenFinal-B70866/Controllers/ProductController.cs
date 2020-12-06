3d2a60f0877b3c8de6d16a764a366c72d0e89b3c
GIANFRANCO.BAGNARELLO <gianfranco.bagnarello@ucr.ac.cr>
2020-12-05 23:58:52 -0600

    Refactoring, limpieza del código del controlador

--- CI0126-ExamenFinal-B70866/Controllers/ProductController.cs
+++ CI0126-ExamenFinal-B70866/Controllers/ProductController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using CI0126_ExamenFinal_B70866.Models;
 using CI0126_ExamenFinal_B70866.Handlers;
