f6383996051c5fbfe6fa0161816a7fe8851cc247
GIANFRANCO.BAGNARELLO <gianfranco.bagnarello@ucr.ac.cr>
2020-12-05 22:36:26 -0600

    Creado método en el controlador para obtener los productos en el carrito de compras.

---
+++ CI0126-ExamenFinal-B70866/Controllers/ShoppingCartController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CI0126_ExamenFinal_B70866.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        [HandleError]
+        public ActionResult shoppingCart()
+        {
+            ShoppingCartHandler shoppingCartHandler = new ShoppingCartHandler();
+            ViewBag.productsInCart = shoppingCartHandler.getAllProductsInCart().ToList<Product>();
+            return View(ViewBag.productsInCart);
+        }
+    }
+}
---
+++ CI0126-ExamenFinal-B70866/SQL Scripts/ShoppingCart.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE ShoppingCart(
+	productID INTEGER PRIMARY KEY IDENTITY,
+	productAmount INTEGER NOT NULL,
+	FOREIGN KEY(productID) REFERENCES Product(productID)
+)
