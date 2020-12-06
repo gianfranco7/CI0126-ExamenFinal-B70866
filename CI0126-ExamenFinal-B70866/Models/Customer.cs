673780a0d3735ba9fb49a32e3f2de1183c9dd6f2
GIANFRANCO.BAGNARELLO <gianfranco.bagnarello@ucr.ac.cr>
2020-12-06 01:55:35 -0600

    Creado modelo para los datos del cliente

---
+++ CI0126-ExamenFinal-B70866/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CI0126_ExamenFinal_B70866.Models
+{
+    public class Customer
+    {
+        [Required(ErrorMessage = "Es necesario que ingrese su dirección para enviar los productos")]
+        [Display(Name = "Ingrese la dirección donde se recibirán los productos")]
+        public string address { get; set; }
+    }
+}
